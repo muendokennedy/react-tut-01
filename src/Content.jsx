e27469e25577547574168a69940a2077aaffb888
muendokennedy <kennedymunyao999@gmail.com>
2024-02-13 23:14:54 +0300

    Learned more about props and prop drilling in react from different components

--- src/Content.jsx
+++ src/Content.jsx
@@ -2,37 +2,8 @@ import React, { useState } from 'react'
 
 import { FaTrashAlt } from 'react-icons/fa'
 
-export const Content = () => {
+export const Content = ({items, handleCheck, handleDelete}) => {
 
-  const [items, setItems] = useState([
-    {
-      id: 1,
-      checked: true,
-      item: "One half pound of Cocoa Covered Almonds Unsalted"
-    },
-    {
-      id: 2,
-      checked: false,
-      item: "Item 2"
-    },
-    {
-      id: 3, 
-      checked: false,
-      item: "item 3"
-    }
-  ]);
-
-  const handleCheck = (id) => {
-    const listItems = items.map((item) => item.id === id ? {...item, checked: !item.checked} : item);
-    setItems(listItems);
-    localStorage.setItem('shoppinglist', JSON.stringify(listItems));
-  }
-
-  const handleDelete = (id) => {
-    const listItems = items.filter((item) => item.id !== id);
-    setItems(listItems);
-    localStorage.setItem('shoppinglist', JSON.stringify(listItems));
-  }
   return (
     <main>
       {items.length ? (
--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 
-export const Footer = () => {
+export const Footer = ({length}) => {
 
   const today = new Date();
 
   return (
     <footer>
-      <p>Copyright &copy; {today.getFullYear()}</p>
+      <p>{length} List {length === 1 ? 'item' : 'items'}</p>
     </footer>
   )
 }
