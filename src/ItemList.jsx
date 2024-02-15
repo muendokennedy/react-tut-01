7161e70fe1239a4622511b7f8d06205f8d5f8745
muendokennedy <kennedymunyao999@gmail.com>
2024-02-15 15:13:04 +0300

    Implemented prop drilling using the ItemList component

---
+++ src/ItemList.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { FaTrashAlt } from 'react-icons/fa'
+
+
+export const ItemList = ({items, handleCheck, handleDelete}) => {
+  return (
+    <div>
+      <ul>
+        {items.map((item) => (
+          <li className='item' key={item.id}>
+            <input type="checkbox" onChange={() => handleCheck(item.id)} checked={item.checked} />
+            <label
+
+            style={(item.checked) ? {textDecoration: 'line-through'} : null}
+
+             onDoubleClick={() => handleCheck(item.id)}>{item.item}</label>
+            <FaTrashAlt onClick={() => handleDelete(item.id)} role='button' tabIndex='0'/>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
---
+++ src/LineItem.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const LineItem = () => {
+  return (
+    <div>LineItem</div>
+  )
+}
