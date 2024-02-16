ddc62c37d9498d618ef0b2eac05b721707f09abe
muendokennedy <kennedymunyao999@gmail.com>
2024-02-16 11:58:58 +0300

    Fixed an issue with the styling of the html elements in the jsx components

--- src/AddItem.jsx
+++ src/AddItem.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 import { FaPlus } from 'react-icons/fa'
 
-export const AddItem = () => {
+export const AddItem = ({newItem, setNewItem, handleSubmit}) => {
   return (
-    <form className='addForm'>
+    <form className='addForm' onSubmit={handleSubmit}>
       <label htmlFor="addItem">Add Item</label>
-      <input type="text" autoFocus id='addItem' placeholder='Add Item' required />
+      <input type="text" autoFocus id='addItem' placeholder='Add Item' required value={newItem} onChange={(e) => setNewItem(e.target.value)} />
       <button type="submit" aria-label='Add Item'>
         <FaPlus/>
       </button>
--- src/ItemList.jsx
+++ src/ItemList.jsx
@@ -4,12 +4,10 @@ import { LineItem } from './LineItem'
 
 export const ItemList = ({items, handleCheck, handleDelete}) => {
   return (
-    <div>
       <ul>
         {items.map((item) => (
           <LineItem key={item.id} item={item} handleCheck={handleCheck} handleDelete={handleDelete}/>
         ))}
       </ul>
-    </div>
   )
 }
